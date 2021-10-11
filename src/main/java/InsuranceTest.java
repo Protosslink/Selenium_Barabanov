12950b7d9365b5ce1de21d08b3c548412366fdcf
LINK-21 <LINK-21@yandex.ru>
2021-10-11 22:44:22 +0300

    123

--- src/main/java/InsuranceTest.java
+++ src/main/java/InsuranceTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class InsuranceTest {
@@ -19,28 +21,42 @@ public class InsuranceTest {
     public void beforeMethod() {
         System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
         baseURL = "https://www.sberbank.ru/ru/person";
+        //baseURL = "https://online.sber.insure/store/travel/";
         driver = new ChromeDriver();
-         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.manage().window().maximize();
         driver.get(baseURL);
+
 
     }
 
     @Test
 
     public void testInsurance() {
+        String mainWindow;
         Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement link = driver.findElement(By.xpath("(//A[@href='/ru/person/bank_inshure/insuranceprogram/life/travel'][text()='Путешествия'])"));
-        WebElement linkTravel = driver.findElement(By.xpath("//A[href='https://online.sber.insure/store/travel/'][text()='Страхование путешественников']"));
 
-        driver.findElement(By.xpath("//a[@role=\"button\" and @aria-label=\"Страхование\"]")).click();
+        mainWindow = driver.getWindowHandle();
+
+        driver.findElement(By.xpath("//a[@role='button' and @aria-label='Страхование']")).click();
         wait.until(ExpectedConditions.visibilityOf(link)).click();
 
-        driver.findElement(By.xpath("//span[@class=\"kitt-button__text\" and text()=\"Оформить онлайн\"]")).click();
-        driver.findElement(By.xpath("//span[@class=\"kitt-button__text\" and text()=\"Оформить на сайте\"]")).click();
+        driver.findElement(By.xpath("//span[@class='kitt-button__text' and text()='Оформить онлайн']")).click();
+        driver.findElement(By.xpath("//span[@class='kitt-button__text' and text()='Оформить на сайте']")).click();
 
-        wait.until(ExpectedConditions.visibilityOf(linkTravel));
+        Set<String> windows = driver.getWindowHandles();
+
+        for(String window: windows){
+            if(!window.equals(mainWindow)){
+                driver.switchTo().window(window);
+            }
+        }
+
         Assert.assertEquals("Ошибка сравнения заголовка вкладки", "Страхование путешественников", driver.getTitle());
+
+        driver.findElement(By.xpath("//div[@class='online-card-program selected']//*[text()='Минимальная']")).click();
+        driver.findElement(By.xpath("//button[@type='button' and text()='Оформить']")).click();
     }
 
     public void fillField(By locator, String value) {
