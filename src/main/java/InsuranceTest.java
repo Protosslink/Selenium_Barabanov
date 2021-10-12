b4c2748c0101d4f8e98b37b2a43d0c6e06bc7df1
LINK-21 <LINK-21@yandex.ru>
2021-10-12 22:16:41 +0300

    123

--- src/main/java/InsuranceTest.java
+++ src/main/java/InsuranceTest.java
@@ -28,11 +28,22 @@ public class InsuranceTest {
         driver.get(baseURL);
 
 
+
+
     }
 
     @Test
 
     public void testInsurance() {
+
+        String firstName = "Дмитрий";
+        String lastName = "Барабанов";
+        String birthDate = "13.03.1989";
+        String passportDateOfIssue = "07.04.2009";
+        String passportSeries = "1111";
+        String passportNumber = "111111";
+        String passportIssue = "Россия";
+
         String mainWindow;
         Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement link = driver.findElement(By.xpath("(//A[@href='/ru/person/bank_inshure/insuranceprogram/life/travel'][text()='Путешествия'])"));
@@ -55,9 +66,44 @@ public class InsuranceTest {
 
         Assert.assertEquals("Ошибка сравнения заголовка вкладки", "Страхование путешественников", driver.getTitle());
 
-        driver.findElement(By.xpath("//div[@class='online-card-program selected']//*[text()='Минимальная']")).click();
+        driver.findElement(By.xpath("//div[@class='online-card-program selected']//*[text()='Минимальная']"));
         driver.findElement(By.xpath("//button[@type='button' and text()='Оформить']")).click();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("documentDate"))));
+
+
+        fillField(By.id("surname_vzr_ins_0"), lastName);
+        fillField(By.id("name_vzr_ins_0"), firstName);
+         fillField(By.id("person_lastName"), lastName);
+        fillField(By.id("person_firstName"), firstName);
+        fillField(By.id("passportSeries"), passportSeries);
+        fillField(By.id("passportNumber"), passportNumber);
+        fillField(By.id("documentIssue"), passportIssue);
+        fillField(By.id("documentDate"), passportDateOfIssue);
+        fillField(By.id("person_birthDate"), birthDate);
+        fillField(By.id("birthDate_vzr_ins_0"), birthDate);
+
+        Assert.assertEquals("Ошибка ввода поля Фамилия",lastName, driver.findElement(By.id("surname_vzr_ins_0")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Имя",firstName,driver.findElement(By.id("name_vzr_ins_0")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Фамилия",lastName,driver.findElement(By.id("person_lastName")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Имя",firstName, driver.findElement(By.id("person_firstName")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Серия паспорта",passportSeries, driver.findElement(By.id("passportSeries")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Номер паспорта",passportNumber, driver.findElement(By.id("passportNumber")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Кем выдан",passportIssue, driver.findElement(By.id("documentIssue")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Дата выдачи",passportDateOfIssue, driver.findElement(By.id("documentDate")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Дата рождения",birthDate, driver.findElement(By.id("person_birthDate")).getAttribute("value"));
+        Assert.assertEquals("Ошибка ввода поля Дата рождения",birthDate, driver.findElement(By.id("birthDate_vzr_ins_0")).getAttribute("value"));
+
+        driver.findElement(By.xpath("//div[@class='btn-group']/label[contains(text(), 'гражданин РФ')]"));
+        driver.findElement(By.xpath("//label[@for=\"checkbox-person_isEmptyMiddleName\"]/span[@class=\"checkbox\"]")).click();
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        System.out.println("qq");
+
+
     }
+
+
 
     public void fillField(By locator, String value) {
         driver.findElement(locator).clear();
